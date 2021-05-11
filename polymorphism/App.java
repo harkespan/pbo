f63a7a402d524644375edb0e101d4d4f8f46067c
Imanuel Harkespan <octone.harry@gmail.com>
2021-05-11 10:55:26 +0700

    Update App.java

--- polymorphism/App.java
+++ polymorphism/App.java
@@ -7,16 +7,10 @@ public class App {
 		BangunDatar bd;
 		SegiEmpat persegi = new SegiEmpat();
 		bd = persegi;
-		bd.lebar = 5;
-		bd.panjang = 10;
 		double luas = bd.luas();
 		bd.cetakLuas("Persegi panjang",luas);
 		Segitiga sikusiku = new Segitiga();
 		bd = sikusiku;
-		bd.panjang = 10;
-		bd.tinggi = 5;
-		double luas1 = bd.luas(bd.panjang, bd.tinggi);
-		bd.cetakLuas("Segitiga siku-siku", luas1);
 	}
 
 }
