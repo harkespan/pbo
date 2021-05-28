69f3b018f30eae2ab357836ba31bbc88d4998a58
Imanuel Harkespan <octone.harry@gmail.com>
2021-05-28 14:17:45 +0700

    create class Lingkaran

---
+++ abstrak/Lingkaran.java
@@ -0,0 +1,17 @@
+package abstrak;
+
+public class Lingkaran extends BangunDatar{
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
