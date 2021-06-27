70e3e42d6fcf9ccbedddddba82b81df7d040c5d9
Imanuel Harkespan <octone.harry@gmail.com>
2021-06-27 21:01:23 +0700

    add table parkir

---
+++ parkir.sql
@@ -0,0 +1,35 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MariaDB
+ Source Server Version : 100136
+ Source Host           : localhost:3306
+ Source Schema         : test
+
+ Target Server Type    : MariaDB
+ Target Server Version : 100136
+ File Encoding         : 65001
+
+ Date: 27/06/2021 20:57:28
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for parkir
+-- ----------------------------
+DROP TABLE IF EXISTS `parkir`;
+CREATE TABLE `parkir`  (
+  `id` int(11) NOT NULL,
+  `nopol` varchar(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `tipe` varchar(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT 'Motor, Mobil, Truk',
+  `tanggal` date NOT NULL,
+  `jam_masuk` time(0) NOT NULL,
+  `jam_keluar` time(0) NULL DEFAULT NULL,
+  `biaya_parkir` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+SET FOREIGN_KEY_CHECKS = 1;
