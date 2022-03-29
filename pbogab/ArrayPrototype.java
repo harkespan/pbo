82b3aadceed7935f0d66b4d5b2ebd1e353f46cab
harkespan <octone.harry@gmail.com>
2022-03-29 21:08:26 +0700

    alter some code

--- pbogab/ArrayPrototype.java
+++ pbogab/ArrayPrototype.java
@@ -4,26 +4,5 @@ import java.util.Scanner;
 public class ArrayPrototype {
 	int[] angka;
 	
-	void setArray(int n) {
-		angka = new int[n];
-		Scanner inp = new Scanner(System.in);
-		for(int i = 0;i < n;i++) {
-			System.out.print("Masukkan Angka ke-"+(i+1)+": ");
-			angka[i] = inp.nextInt();
-		}
-	}
-	
-	void cetakArray() {
-		System.out.println("Isi array: ");
-		int i = 0;
-		while(angka.length>0) {	
-			if(i==angka.length)
-			{
-				break;
-			}
-
-			System.out.print(angka[i]+",");
-			i++;
-		}
-	}
+	//create method here
 }
