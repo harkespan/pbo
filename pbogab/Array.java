f6409564bca6a4391ad8389e9eef1bdcbc7ab503
harkespan <octone.harry@gmail.com>
2022-03-29 21:06:06 +0700

    type some code

--- pbogab/Array.java
+++ pbogab/Array.java
@@ -1,5 +1,13 @@
 package pbogab;
+import java.util.Scanner;
 
 public class Array {
+	public static void main(String[] args) {
+		int jmlData;
+		Scanner inp = new Scanner(System.in);
+		System.out.print("Masukkan Jumlah Data: ");
+		jmlData = inp.nextInt();
 
+		//the rest of code
+	}
 }
