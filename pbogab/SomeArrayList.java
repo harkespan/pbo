3cecc9c01eb64ae247d2055b151f19b97aa36b57
harkespan <octone.harry@gmail.com>
2022-03-30 10:37:11 +0700

    add input with scanner

--- pbogab/SomeArrayList.java
+++ pbogab/SomeArrayList.java
@@ -1,6 +1,8 @@
 package pbogab;
 import java.util.ArrayList;
 import java.lang.Integer;
+import java.util.Scanner;
+
 public class SomeArrayList {
 	public static void main(String[] args)
 	{
@@ -8,8 +10,22 @@ public class SomeArrayList {
 		i.add(1);
 		i.add(10);
 		i.add(9);
-		i.set(1,8);
+		i.set(1,8);//menimpa nilai pada indeks 1 yang tadinya 10 menjadi 8
 		System.out.println(i);
 		System.out.println(i.get(1));
+		
+		Scanner inp = new Scanner(System.in);
+		System.out.print("Masukkan angka: ");
+		int x = inp.nextInt();
+		
+		i.add(x);
+		
+		int jmlData = i.size(); //untuk mencari jumlah data pada arraylist
+		
+		for(int a = 0;a < jmlData;a++)
+		{
+			System.out.println(i.get(a));
+		}
+		
 	}
 }
