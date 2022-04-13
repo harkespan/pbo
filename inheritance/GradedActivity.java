6d2ee07ff9add2d3dcd210d220694a8889bd63f8
Imanuel Harkespan <octone.harry@gmail.com>
2022-04-13 08:10:13 +0700

    add new file in inheritance package

---
+++ inheritance/GradedActivity.java
@@ -0,0 +1,31 @@
+package inheritance;
+
+public class GradedActivity {
+	double score;
+	String name;
+	
+	public GradedActivity()
+	{
+		
+	}
+	
+	public GradedActivity(String n)
+	{
+		
+	}
+	
+	public void setScore(double s)
+	{
+		
+	}
+	
+	public double getScore()
+	{
+		return 0;
+	}
+	
+	public char getGrade()
+	{
+		return ' ';	
+	}
+}
---
+++ inheritance/PassFailExam.java
@@ -0,0 +1,5 @@
+package inheritance;
+
+public class PassFailExam extends PassFailActivity {
+
+}
